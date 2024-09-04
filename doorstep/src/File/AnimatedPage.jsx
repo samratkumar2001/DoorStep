2abc29b12a2ff1abb051cf64ac16c15969583f96
Parthib <palparthib97@gmail.com>
2024-09-04 11:18:59 +0530

    2nd change

--- doorstep/src/File/AnimatedPage.jsx
+++ doorstep/src/File/AnimatedPage.jsx
@@ -7,7 +7,7 @@ const animations = {
     exit:{ opacity: 0, x:-100}
 };
 
-const AnimatedPage = ({children}) => {
+const AnimatedPag = ({children}) => {
     return(
         <motion.div
         variants={animations}
@@ -21,4 +21,4 @@ const AnimatedPage = ({children}) => {
     );
 };
 
-export default AnimatedPage;
+export default AnimatedPag;
